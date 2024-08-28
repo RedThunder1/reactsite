9b8f93a44028b572e6381c399721b36db201dbb5
James <redthunderspartan@gmail.com>
2024-08-28 16:19:54 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+// @ts-ignore
+import logo from './SpartanLogo.png';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import './App.css';
 
+import About from './About'
+
+
 function App() {
+    const navigate
+
+    function AboutButton() {
+
+    }
+
+    function ProjectsButton() {
+
+    }
+
+    function ContactButton() {
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+          <img src={logo} alt="logo" className="logo"></img>
+          <div className="navbar_top">
+              <button type="button" onClick={ProjectsButton}>Projects</button>
+              <button type="button" onClick={ContactButton}>Contact</button>
+          </div>
       </header>
+        <main className="main">
+            <div className="name">
+              <h1>James Haver</h1>
+              <button type="button" onClick={AboutButton}>About</button>
+          </div>
+
+      </main>
+
+        <footer className="footer">
+          <p>Footer</p>
+      </footer>
     </div>
   );
 }
